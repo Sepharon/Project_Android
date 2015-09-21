0d66c2b47cd63a51ecaa9d740c3ef8c83cde079d
sergi.carol <sergi.carol@71361cdc-cf55-43f4-919b-9ce784074d33>
2015-09-21 11:44:31 +0000

    Added receiving msg in smartphone in a good way:
    NEED STILL TO FIX GARBAGE AT THE END OF MESSAGE

--- DroneControl/app/src/main/java/com/iha/group2/dronecontrol/DroneControl.java
+++ DroneControl/app/src/main/java/com/iha/group2/dronecontrol/DroneControl.java
@@ -1,9 +1,16 @@
 package com.iha.group2.dronecontrol;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+// TODO : NOT IMPLEMENTED YET
 
 public class DroneControl extends AppCompatActivity {
 
@@ -11,6 +18,54 @@ public class DroneControl extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drone_control);
+
+        Intent in = getIntent();
+        final String ip = in.getStringExtra("ip");
+        Button forward = (Button) findViewById(R.id.forward);
+        Button backward = (Button) findViewById(R.id.backward);
+        Button right = (Button) findViewById(R.id.Right);
+        Button left = (Button) findViewById(R.id.Left);
+
+        Button up = (Button) findViewById(R.id.up_button);
+        Button down = (Button) findViewById(R.id.down_button);
+
+        Log.v("Drone Control ip: ",ip);
+        forward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                send_data("F", ip,"");
+            }
+        });
+        backward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                send_data("B",ip,"");
+            }
+        });
+        right.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                send_data("R",ip,"");
+            }
+        });
+        left.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                send_data("L",ip,"");
+            }
+        });
+        up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                send_data("U",ip,"");
+            }
+        });
+        down.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                send_data("D",ip,"");
+            }
+        });
     }
 
     @Override
@@ -33,5 +88,14 @@ public class DroneControl extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void send_data(String v,String ip, String action){
+        Intent intent = new Intent(getBaseContext(),UDPconnection.class);
+        intent.putExtra("value",v);
+        intent.putExtra("ip",ip);
+        intent.putExtra("action",action);
+        startService(intent);
+
     }
 }
