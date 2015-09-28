93948a972fe9533d33b39a03b1d778f0da602303
group 14 <sergicarol35@gmail.com>
2015-09-28 17:33:12 +0200

    Created streaming activity

---
+++ app/src/main/java/com/iha/group2/dronecontrol/Streaming_camera.java
@@ -0,0 +1,27 @@
+package com.iha.group2.dronecontrol;
+
+import android.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+
+public class Streaming_camera extends AppCompatActivity {
+
+    static final String url = "http://10.192.86.63:8080/video";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_streaming_camera);
+
+
+
+        WebView browser = (WebView) findViewById(R.id.webView);
+
+        browser.getSettings().setLoadWithOverviewMode(true);
+        browser.getSettings().setUseWideViewPort(true);
+
+        browser.loadUrl(url);
+    }
+}
