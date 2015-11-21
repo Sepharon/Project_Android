7c040c8c48a5650533533b30db55d6172865fd16
group 14 <sergicarol35@gmail.com>
2015-11-21 16:18:26 +0100

    Improvements done to the arduino_emulator

--- arduino_emulator.py
+++ arduino_emulator.py
@@ -3,8 +3,12 @@ import sys
 import select
 import Queue
 import subprocess
+import atexit
 
 PORT = 8888
+
+def _exit():
+    print "Thank you for using our program"
 
 def promp_me(msg) :
     #sys.stdout.write('<You> ')
@@ -20,30 +24,33 @@ def servidor():
     s.bind(('',PORT))
     s.settimeout(2)
     while True:
-        socket_list = [sys.stdin, s]
-        read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
-        for sock in read_sockets:
-            #incoming message from remote server
-            if sock == s:
-                data, addr = sock.recvfrom(1024)
-                if not data :
-                    print '\nDisconnected from chat server'
-                    sys.exit()
-                else :
-                    promp_him(data)
-                    if data == "GPS":
-                        s.sendto("GPS!54.322;32.123;2.3;4.5;",addr)
-                        print "\nGPS!54.322;32.123;2.3;4.5;\n"
-                    elif data == "Weather":
-                        s.sendto("Weather!54.322;32.123;091123;400.3",addr)
-                        print "\nWeather!54.322;32.123;091123;400.3\n"
-                    elif data == "Stop":
-                        s.sendto("OK",addr)
-                        print "\nOK\n"
-                    elif data == "connect":
-                        print "alive"
-                        s.sendto("alive\n",addr)
-                    
+        try:
+            socket_list = [sys.stdin, s]
+            read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
+            for sock in read_sockets:
+                #incoming message from remote server
+                if sock == s:
+                    data, addr = sock.recvfrom(1024)
+                    if not data :
+                        print '\nDisconnected from chat server'
+                        sys.exit()
+                    else :
+                        promp_him(data)
+                        if data == "GPS":
+                            s.sendto("GPS!54.322;32.123;2.3;4.5;",addr)
+                            print "\nGPS!54.322;32.123;2.3;4.5;\n"
+                        elif data == "Weather":
+                            s.sendto("Weather!54.322;32.123;091123;400.3",addr)
+                            print "\nWeather!54.322;32.123;091123;400.3\n"
+                        elif data == "Stop":
+                            s.sendto("OK",addr)
+                            print "\nOK\n"
+                        elif data == "connect":
+                            print "alive"
+                            s.sendto("alive\n",addr)
+        except KeyboardInterrupt:
+            print "Shutdown requested...exiting"
+            break
 
 if __name__ == "__main__":
     
@@ -58,7 +65,7 @@ if __name__ == "__main__":
     out = IP.communicate()[0]
     if out != "":
         print "eth0 " + out
-    
+    atexit.register(_exit)
     servidor()
     
 
